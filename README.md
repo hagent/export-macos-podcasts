f8c2d544fe79b8cd04ab26513435761e0748b7e3
Alexey Tomin <alextomin@list.ru>
2021-06-27 01:41:42 +0200

    Macos podcasts export script

---
+++ README.md
@@ -0,0 +1,11 @@
+Since mac os 11.4 apple official podcasts app doesn't allow to drag and drop downloaded files. This simple script tries to resolve this problem. It can export apple podcasts downloaded or cached mp3 files from "app's cache folder" to "~/Downloads/PodcastsExport/[CURRENT_DATA]" folder with readable file names - podcast titles instead of uuids like "EEFE102A-2134-4FBC-BB14-44FC18736FEE". Script tries to fetch podcast titles from podcasts app database if something went wrong it takes mp3 metadata titles and it fails too script just copies files with original filenames.
+
+
+Requirements: Node.js version >= 14
+
+Usage:
+1. Clone/Download repository
+2. Open terminal
+3. Go to project folder `cd [REPOSITORY FOLDER]`
+4. Execute `chmod u+x ./export.sh && ./export.sh`
+5. Next time it's possible just to run `npm start` command
---
+++ index.js
@@ -0,0 +1,113 @@
+const { promises: fs } = require("fs");
+const { promisify } = require("util");
+const sqlite3 = require("sqlite3").verbose();
+const mm = require("music-metadata");
+const path = require("path");
+const { exec } = require("child_process");
+
+const d = new Date();
+const pad = (s) => s.toString().padStart(2, "0");
+const month = pad(d.getMonth() + 1);
+const day = pad(d.getDate());
+const currentDateFolder = `${d.getFullYear()}.${month}.${day}`;
+const outputDir = `${process.env.HOME}/Downloads/PodcastsExport/${currentDateFolder}`;
+const podcastSelectSQL = `
+  SELECT zcleanedtitle as zcleanedtitle, zuuid as zuuid
+    FROM ZMTEPISODE;
+`;
+const fileNameMaxLength = 50;
+
+async function getPodcastsBasePath() {
+  const groupContainersFolder = `${process.env.HOME}/Library/Group Containers`;
+  try {
+    const libraryGroupContainersDirList = await fs.readdir(
+      groupContainersFolder
+    );
+    const podcastsAppFolder = libraryGroupContainersDirList.find((d) =>
+      d.includes("groups.com.apple.podcasts")
+    );
+    if (!podcastsAppFolder) {
+      throw new Error(
+        `Could not find podcasis app folder in ${groupContainersFolder}`
+      );
+    }
+    return `${process.env.HOME}/Library/Group Containers/${podcastsAppFolder}`;
+  } catch (e) {
+    throw new Error(
+      `Could not find podcasis app folder in ${groupContainersFolder}, original error: ${e}`
+    );
+  }
+}
+
+async function getPodcastsDBPath() {
+  return `${await getPodcastsBasePath()}/Documents/MTLibrary.sqlite`;
+}
+
+async function getPodcastsCacheFilesPath() {
+  return `${await getPodcastsBasePath()}/Library/Cache`;
+}
+
+async function getDBPodcastsData() {
+  try {
+    const dbOrigin = new sqlite3.Database(await getPodcastsDBPath());
+    const db = {
+      serialize: promisify(dbOrigin.serialize).bind(dbOrigin),
+      all: promisify(dbOrigin.all).bind(dbOrigin),
+      close: promisify(dbOrigin.close).bind(dbOrigin),
+    };
+
+    await db.serialize();
+    return await db.all(podcastSelectSQL);
+  } catch (error) {
+    console.error("Could not fetch data from podcasts database:", error);
+    return [];
+  } finally {
+    try {
+      db.close();
+    } catch {}
+  }
+}
+
+async function getMP3Title(path) {
+  const metadata = await mm.parseFile(path);
+  return metadata?.common?.title;
+}
+
+async function exportPodcasts(podcastsDBData) {
+  const cacheFilesPath = await getPodcastsCacheFilesPath();
+  const podcastFiles = await fs.readdir(cacheFilesPath);
+  const podcastMP3Files = podcastFiles.filter((f) => f.includes(".mp3"));
+  const filesWithDBData = podcastMP3Files.map((fileName) => {
+    const uuid = fileName.replace(".mp3", "");
+    const meta = podcastsDBData.find((m) => m.zuuid === uuid);
+    return {
+      fileName,
+      uuid,
+      path: `${cacheFilesPath}/${fileName}`,
+      meta,
+    };
+  });
+
+  await fs.mkdir(outputDir, { recursive: true });
+  await Promise.all(
+    filesWithDBData.map(async (podcast) => {
+      const newFileName =
+        podcast.meta?.zcleanedtitle ??
+        (await getMP3Title(podcast.path)) ??
+        podcast.uuid;
+      const newFileNameLength = newFileName.substr(0, fileNameMaxLength);
+      const newPath = `${outputDir}/${newFileNameLength}.mp3`;
+      console.log(`${podcast.path} -> ${newPath}`);
+      await fs.copyFile(podcast.path, newPath);
+    })
+  );
+  console.log(`\n\nSuccessful Export to '${outputDir}' folder!`);
+  exec(`open ${outputDir}`);
+}
+
+async function main() {
+  const dbPodcastData = await getDBPodcastsData();
+  await exportPodcasts(dbPodcastData);
+}
+
+main();
