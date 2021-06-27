1cb7d077072e008e8a446ae69cf4ce57b4e5c41f
Alexey Tomin <alextomin@list.ru>
2021-06-27 10:51:43 +0200

    Clear up bash and .npmrc version checks, it's in js now

--- export.sh
+++ export.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
 
-NODE_VER_FULL=$(node -v)
-NODE_VER=${NODE_VER_FULL:1:2}
-
-if [[ "$NODE_VER" = *.* ]]; then
-  echo "your nodejs version < 14, please install >= 14 https://nodejs.org/en/download/"
-  exit 1
-fi
-
-if [ -z "$NODE_VER" ]; then
-  echo "please install nodejs version >=14 https://nodejs.org/en/download/"
-  exit 1;
-fi
-
-if (( NODE_VER < 14 )); then
-  echo "your nodejs version < 14, please install >= 14 https://nodejs.org/en/download/"
-  exit 1
-  
-else
-  npm install
-  npm start
-fi
+npm install
+npm start
